adce2f10adb64e5d73a67aecdb896492b2cf44a5
Noah S <Noah3141@gmail.com>
2023-06-02 15:29:00 -0400

    example structure

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use rocket_dyn_templates::Template;
-
 #[macro_use] extern crate rocket; 
 
 #[rocket::main]
@@ -8,7 +6,6 @@ async fn main() -> Result<(), rocket::Error> {
     let _rocket = rocket::build()
         .mount("/", routes![
         ])
-        .attach(Template::fairing())
         .launch()
         .await?;
 
--- src/pages/index.rs
+++
@@ -1,7 +0,0 @@
-use rocket_dyn_templates::Template;
-
-#[get("/index")]
-pub fn index() -> Template {
-    let context = serde_json::json!({"logged-in": false});
-    Template::render("index", &context)
-}
