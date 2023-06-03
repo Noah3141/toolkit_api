aee3de2ad4fd8d3a6a2fc33ab11832ad9e19b78a
Noah S <Noah3141@gmail.com>
2023-06-03 14:00:30 -0400

    preparing security features

---
+++ src/security/auth.rs
@@ -0,0 +1,16 @@
+use rocket::serde::json::Json;
+
+
+struct Auth {
+    name: String,
+    token: String,
+}
+
+#[get("/auth")]
+pub fn first_time_auth() -> Json<MyStruct> {
+
+    todo!();
+    
+
+    Json(my_struct)
+}
