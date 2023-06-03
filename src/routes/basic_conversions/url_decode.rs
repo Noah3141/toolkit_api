f5d83b7587fc4f9d34e6b9cde99a8acb6ca62f21
Noah S <Noah3141@gmail.com>
2023-06-03 13:59:10 -0400

    url decoding

--- src/routes/basic_conversions/url_decode.rs
+++ src/routes/basic_conversions/url_decode.rs
@@ -1,21 +1,16 @@
-use std::borrow::Cow;
-
-
 use {
     rocket::{serde::{json::Json, Deserialize}},
     urlencoding::decode
 };
 
 #[get("/convert/urldecode/<req>")]
-pub fn url_decode(req: String) -> String {
+pub async fn url_decode(req: String) -> String {
     let content = req;
-    
 
     let decoded = match decode(&content) {
         Ok(decoded) => decoded.into_owned(),
         Err(e) => e.utf8_error().to_string() 
     };
-
 
     let res = decoded;
     String::from(res)
