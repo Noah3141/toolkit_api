30d43d1a749f6caa6c17e423f4ca793835f7247c
Noah S <Noah3141@gmail.com>
2023-06-20 12:00:07 -0400

    Remove redundant filter

--- src/routes/russian/utils/input_parsing.rs
+++ src/routes/russian/utils/input_parsing.rs
@@ -2,9 +2,6 @@
 pub fn wordify(input_text: String) -> Vec<String> {
 
 
-    let input_text: String = input_text
-        .replace("\n", "");
-
     let mut words = vec![];
     for word in input_text.split_whitespace() {
         words.push(word.to_string().to_lowercase());
