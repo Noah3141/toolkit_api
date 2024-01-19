a7ad8a904587795a8e7a3ff753ca53cafbde6d46
Noah Steckley <Noah3141@gmail.com>
2024-01-18 20:26:45 -0800

    блять

--- src/utils/cors.rs
+++ src/utils/cors.rs
@@ -17,6 +17,7 @@ impl Fairing for Cors {
 
     async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
         response.set_header(Header::new("Access-Control-Allow-Origin", "https://rubit.vercel.app/"));
+        response.set_header(Header::new("Vary", "Origin"));
         response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, DELETE"));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
     }
