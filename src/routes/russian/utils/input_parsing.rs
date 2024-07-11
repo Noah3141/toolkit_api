c8fd9495c80e772aa43bb3f5edb9d69f5c4e16a3
Noah Steckley <Noah3141@gmail.com>
2024-07-10 20:37:31 -0400

    Removing whitespace

--- src/routes/russian/utils/input_parsing.rs
+++ src/routes/russian/utils/input_parsing.rs
@@ -1,7 +1,5 @@
 
 pub fn wordify(input_text: String) -> Vec<String> {
-
-
     let mut words = vec![];
     for word in input_text.split_whitespace() {
         words.push(word.to_string().to_lowercase());
--- src/routes/russian/utils/stop_words.rs
+++ src/routes/russian/utils/stop_words.rs
@@ -1,8 +1,6 @@
 pub fn remove_stop_words(word_list: Vec<String>) -> Vec<String> {
-
     let stop_words = std::fs::read_to_string("./src/routes/russian/utils/stop_words.txt").expect("presence of stop_words.txt");
-
-    let mut checked_list: Vec<String> = vec![];
+    let mut checked_list: Vec<String> = vec![String; word_list.len()];
     for word in word_list {
         if !stop_words.contains(&word) {
             checked_list.push(word)
